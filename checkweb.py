bad4c9691a57135dd48cbb6c83d98c0676dd8cbd
deng-xiang <john_dx@163.com>
2020-09-14 18:00:07 +0800

    调试解析SOE网站的html报文

--- checkweb.py
+++ checkweb.py
@@ -1,5 +1,7 @@
 import requests
 import openpyxl
+
+from bs4 import BeautifulSoup
 from openpyxl.styles import PatternFill, Border, Side, Font, Alignment
 
 def check(rul):
@@ -16,13 +18,50 @@ sheet=wb['web']
 
 for row in range(2,sheet.max_row+1):
     checkurl=sheet['C'+str(row)].value
-    sheet['D'+str(row)].value=check( checkurl )
+#    sheet['D'+str(row)].value=check( checkurl )
+    print(str(sheet['B'+str(row)].value) +':'+ str(sheet['D'+str(row)].value) )
 
 wb.save(file_name)
 
 
 
-recode=check('http://www.jb51.net')
-print(recode)
-recode=check('http://www.glswxgj.gov.cn/')
-print(recode)
+
+
+url='http://seo.chinaz.com/'
+postdata = {
+"q":"www.guilin.gov.cn"
+}
+r = requests.post(url, postdata)
+#print (r.text)
+
+soup = BeautifulSoup(r.text, 'html.parser')
+#网站域名
+
+url=soup.find('div',attrs={'class':'ball color-63'})
+print("网站域名:"+url.string)
+#备案信息
+document=soup.find('td',attrs={'class':'_chinaz-seo-newtc _chinaz-seo-newh40'})
+#print(document.span.a)
+document=document.find('span')
+document=document.find('i')
+document=document.find('a')
+print("备案号:"+document.string)
+#IP
+IP=soup.find('td',attrs={'class':'_chinaz-seo-newh78 _chinaz-seo-newinfo'})
+
+IP=IP.find('div',attrs={'class':'pb5'})
+IP=IP.find('span')
+IP=IP.find('i')
+IP=IP.find('a')
+ip1=IP.string.split('[')[0]
+addr=IP.string.split('[')[1].replace(']','')
+print("IP:"+ip1)
+print("物理位置:"+addr)
+
+
+
+
+
+
+
+
